ed9ef5c78c456403712b7595d794acd0f2af14c7
acll19 <acll19@gmail.com>
2022-01-29 17:00:45 -0500

    attaches additional (and required) subnet to alb

--- infra/alb.tf
+++ infra/alb.tf
@@ -1,6 +1,9 @@
 resource "aws_lb" "versionn-app-alb" {
-  name               = "test-lb-tf"
+  name               = "versionn-app-alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = [aws_subnet.versionn-app-subnet.id]
+  subnets            = [
+      aws_subnet.versionn-app-subnet-a.id,
+      aws_subnet.versionn-app-subnet-b.id
+  ]
 }
--- infra/network.tf
+++ infra/network.tf
@@ -1,8 +1,21 @@
+locals {
+  availability_zone_a = "${var.region}a"
+  availability_zone_b = "${var.region}b"
+}
+
+
 resource "aws_vpc" "versionn-app-vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "versionn-app-subnet" {
-  vpc_id     = aws_vpc.versionn-app-vpc.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "versionn-app-subnet-a" {
+  vpc_id            = aws_vpc.versionn-app-vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = local.availability_zone_a
+}
+
+resource "aws_subnet" "versionn-app-subnet-b" {
+  vpc_id            = aws_vpc.versionn-app-vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = local.availability_zone_b
 }
