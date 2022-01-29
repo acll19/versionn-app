3bb2763d384dc36024e19b391c238406e0626dcb
acll19 <acll19@gmail.com>
2022-01-29 15:30:24 -0500

    sets target type to ip

--- infra/aws-lb-target-group.tf
+++ infra/aws-lb-target-group.tf
@@ -3,5 +3,5 @@ resource "aws_lb_target_group" "version-app-target-group" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.versionn-app-vpc.id
-  target_type = "alb" 
+  target_type = "ip" 
 }
