13cc97b7568c428a2221cfd886a7c99e16d2b45b
acll19 <acll19@gmail.com>
2022-01-29 17:51:46 -0500

    format changes

--- infra/network.tf
+++ infra/network.tf
@@ -20,22 +20,22 @@ resource "aws_subnet" "versionn-app-subnet-b" {
   availability_zone = local.availability_zone_b
 }
 
-resource "aws_internet_gateway" "versionn-app_ecs_igw" {
+resource "aws_internet_gateway" "versionn-app-ecs-igw" {
   vpc_id = "${aws_vpc.versionn-app-vpc.id}"
 }
 
 
-resource "aws_route_table" "versionn-app_route_table" {
+resource "aws_route_table" "versionn-app-route-table" {
   vpc_id = "${aws_vpc.versionn-app-vpc.id}"
 }
 
-resource aws_route "versionn-app_public_route" {
-  route_table_id         = "${aws_route_table.versionn-app_route_table.id}"
+resource aws_route "versionn-app-public-route" {
+  route_table_id         = "${aws_route_table.versionn-app-route-table.id}"
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.versionn-app_igw.id}"
+  gateway_id             = "${aws_internet_gateway.versionn-app-ecs-igw.id}"
 }
 
-resource "aws_route_table_association" "versionn-app_route_table_association1" {
+resource "aws_route_table_association" "versionn-app-route-table-association-public" {
   subnet_id      = "${aws_subnet.versionn-app-subnet-public.id}"
-  route_table_id = "${aws_route_table.versionn-app_route_table.id}"
+  route_table_id = "${aws_route_table.versionn-app-route-table.id}"
 }
