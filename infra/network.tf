39c2de34ba10636c82d31f9956fac17a52784cba
acll19 <acll19@gmail.com>
2022-01-29 17:08:14 -0500

    (oops) fixes confliction subnet cidr blocks

--- infra/network.tf
+++ infra/network.tf
@@ -16,6 +16,6 @@ resource "aws_subnet" "versionn-app-subnet-a" {
 
 resource "aws_subnet" "versionn-app-subnet-b" {
   vpc_id            = aws_vpc.versionn-app-vpc.id
-  cidr_block        = "10.0.0.0/24"
+  cidr_block        = "10.0.1.0/24"
   availability_zone = local.availability_zone_b
 }
