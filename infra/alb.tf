57a56ac125b687c4751064d3dc29395537e1665e
acll19 <acll19@gmail.com>
2022-01-29 17:44:58 -0500

    fixes unreferenced subnet issue

--- infra/alb.tf
+++ infra/alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "versionn-app-alb" {
   internal           = false
   load_balancer_type = "application"
   subnets            = [
-      aws_subnet.versionn-app-subnet-a.id,
+      aws_subnet.versionn-app-subnet-public.id,
       aws_subnet.versionn-app-subnet-b.id
   ]
 }
