a7027e54ff0a5278fc866aa8840ffba8512f3d79
acll19 <acll19@gmail.com>
2022-01-29 18:52:36 -0500

    makes ecs service depend on target group explicitly

--- infra/aws-lb-target-group.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_lb_target_group" "version-app-target-group" {
-  name        = "${var.app_prefix}-lb-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.versionn-app-vpc.id
-  target_type = "ip"
-  depends_on = [aws_lb.versionn-app-alb]
-}
