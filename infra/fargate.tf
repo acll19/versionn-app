684a9d9108b02ca3f763b3ec44bca59816309076
acll19 <acll19@gmail.com>
2022-01-28 20:39:14 -0500

    Adds ECS fargate cluster & task definition

---
+++ infra/fargate.tf
@@ -0,0 +1,34 @@
+
+locals {
+  image  = "acll19/version-app"
+}
+
+
+resource "aws_ecs_cluster" "versionn-app-ecs-cluster" {
+  name = "${var.app_prefix}-ECSCluster"
+
+  tags = {
+    Name = "${var.app_prefix}-versionn-app-ecs-cluster"
+  }
+}
+
+resource "aws_ecs_task_definition" "versionn-app-task" {
+    family                   = "${var.app_prefix}-ECSTaskDefinition"
+    requires_compatibilities = ["FARGATE"]
+    network_mode             = "awsvpc"
+    cpu                      = "${var.app_cpu_limit}"
+    memory                   = ${var.app_memory_limit}"
+    container_definitions    = jsonencode([
+        {
+            name         = "${var.app_prefix}"
+            image        = "${var.app_image_name}"
+            cpu          = var.app_cpu_limitu
+            memory       = var.app_memory_limit
+            essential    = true
+            portMappings = [{
+                ontainerPort = 8080
+                hostPort     = 80
+            }]
+        }
+    ])
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,14 +1,28 @@
 variable "region" {
   description = "AWS region"
-  default = "us-east-2"
+  default     = "us-east-2"
 }
 
 variable "aws_profile" {
   description = "AWS profile"
-  default = "default"
+  default     = "default"
 }
 
 variable "app_prefix" {
   description = "Application prefix for the AWS services that are built"
-  default = "versionn-app"
+  default     = "versionn-app"
+}
+
+variable "app_cpu_limit" {
+    description = "AWS ECS CPU limit for the versionn-app task definition"
+    default = 256
+}
+
+variable "app_memory_limit" {
+    description = "AWS ECS Memory limit for the versionn-app task definition"
+    default = 512
+}
+
+variable "app_image_name" {
+    description = "Docker image name"
 }
