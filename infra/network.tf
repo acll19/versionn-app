018e07671a8bf40f4ef25b4eeab7dbeff50e0da9
acll19 <acll19@gmail.com>
2022-01-29 17:19:45 -0500

    attatches internet gateway to VPC

--- infra/network.tf
+++ infra/network.tf
@@ -19,3 +19,7 @@ resource "aws_subnet" "versionn-app-subnet-b" {
   cidr_block        = "10.0.1.0/24"
   availability_zone = local.availability_zone_b
 }
+
+resource "aws_internet_gateway" "versionn-app_ecs_igw" {
+  vpc_id = "${aws_vpc.versionn-app-vpc.id}"
+}
