ddbfe4590b574e37eb5441103783ca6a9d47acaf
acll19 <acll19@gmail.com>
2022-01-30 13:53:00 -0500

    uses log group name in ecs task definition

--- infra/fargate.tf
+++ infra/fargate.tf
@@ -27,7 +27,7 @@ resource "aws_ecs_task_definition" "versionn-app-task" {
             "logConfiguration": {
                 "logDriver": "awslogs",
                 "options": {
-                    "awslogs-group": "${aws_cloudwatch_log_group.versionn-app-task-log-group}",
+                    "awslogs-group": "${aws_cloudwatch_log_group.versionn-app-task-log-group.name}",
                     "awslogs-region": "${var.region}",
                     "awslogs-stream-prefix": "ecs"
                 } 
