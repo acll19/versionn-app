138d08f639556a9b021a543c9539fcea0ae39b59
acll19 <acll19@gmail.com>
2022-01-30 14:45:49 -0500

    Defines aws_iam_role_policy

--- infra/authorization.tf
+++ infra/authorization.tf
@@ -6,15 +6,31 @@ resource "aws_iam_role" "versionn-app-role" {
     Effect = "Allow"
     Statement = [
       {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "versionn-app-policy" {
+  name = "${var.app_prefix}-awslogs"
+  role = aws_iam_role.versionn-app-role.id
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
         Action = [
             "logs:CreateLogGroup",
             "logs:CreateLogStream",
             "logs:PutLogEvents",
             "logs:DescribeLogStreams"
         ]
-        Resource = [
-          "arn:aws:logs:*:*:*"
-        ]
+        Resource = "*"
       },
     ]
   })
