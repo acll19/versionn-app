b790b2e58c8140519651401c6b4da842541672bd
acll19 <acll19@gmail.com>
2022-01-30 12:37:36 -0500

    configures network_configuration for verssionn-app-service

--- infra/fargate.tf
+++ infra/fargate.tf
@@ -26,3 +26,20 @@ resource "aws_ecs_task_definition" "versionn-app-task" {
         }
     ])
 }
+
+resource "aws_ecs_service" "versionn-app-service" {
+  name            = "${var.app_prefix}-service"
+  cluster         = aws_ecs_cluster.versionn-app-ecs-cluster.id
+  task_definition = aws_ecs_task_definition.versionn-app-task.arn
+  desired_count   = 2
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.version-app-target-group.arn
+    container_name   = "${var.app_prefix}-app"
+    container_port   = var.container_port
+  }
+
+  network_configuration {
+      subnets: [aws_subnet.versionn-app-subnet-public, aws_subnet.versionn-app-subnet-b]
+  }
+}
