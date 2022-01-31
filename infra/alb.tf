20e7a5e37b598cf0e1d1ed49aa7c300137e064bd
acll19 <acll19@gmail.com>
2022-01-30 20:06:16 -0500

    sets security group to configure external traffic

--- infra/alb.tf
+++ infra/alb.tf
@@ -1,7 +1,27 @@
+resource "aws_security_group" "versionn-app-alb-security-group" {
+  name        = "${var.app_prefix}-security-group"
+  vpc_id      = "${aws_vpc.versionn-app-vpc.id}"
+
+  ingress {
+    from_port   = var.container_port
+    to_port     = var.container_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_lb" "versionn-app-alb" {
   name               = "versionn-app-alb"
   internal           = false
   load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.versionn-app-alb-security-group.id}"]
   subnets            = [
       aws_subnet.versionn-app-subnet-public.id,
       aws_subnet.versionn-app-subnet-b.id
